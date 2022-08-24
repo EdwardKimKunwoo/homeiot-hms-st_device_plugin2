e0938a2015a0255f0005c13ca0db0a1130086384
cholhan-park <cholhan.park@samsung.com>
2022-08-24 13:19:58 +0900

    2.9.13 -> 2.9.14

--- sds_2.9.11/plugin/js/main.js
+++ sds_2.9.11/plugin/js/main.js
@@ -12,7 +12,7 @@ SAMSUNG shall not be liable for any damages suffered by licensee as a result of 
 /******************************************************************************
  * Configure Value
  *******************************************************************************/
-var HNDevicePluginVersion = "2.9.12"; // plugin : 2.9.12, ST app : 1.7.84.21, Date : May 27 2022
+var HNDevicePluginVersion = "2.9.14"; // plugin : 2.9.14, ST app : 1.7.86.21, Date : May 27 2022
 var LocaleCode = getPreferredLocaleCode(navigator.language);
 
 window.VERBOSE_LOG_FLAG = false;    //debug log option
